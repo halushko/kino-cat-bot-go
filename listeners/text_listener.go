e81b3d0cbf2f06600e91350efeeab68fc00e3455
Dmytro Galushko <galushko@netcracker.com>
2024-10-27 10:47:14 +0200

    Refactor. Use v1.15.2 common lib

--- listeners/text_listener.go
+++ listeners/text_listener.go
@@ -17,10 +17,10 @@ func StartTextMessagesSender(bot *telebot.Bot) {
 			return
 		}
 
-		log.Printf("[StartTextMessagesSender] Парсинг повідомлення: chatID = %d, message = %s", userId, messageText) // Новый лог для проверки данных
+		log.Printf("[StartTextMessagesSender] Парсинг повідомлення: chatID = %d, message = %s", userId, messageText)
 
 		if userId != 0 && messageText != "" {
-			_, err := bot.Send(&telebot.User{ID: userId}, messageText)
+			_, err = bot.Send(&telebot.User{ID: userId}, messageText)
 			if err != nil {
 				log.Printf("[StartTextMessagesSender] Помилка при відправленні повідомлення користувачу: %v", err)
 			} else {
